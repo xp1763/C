73c2d9ea124a5918195c89b84788583c6e5d9b06
xp1763 <yuhui1763@gmail.com>
2023-02-21 15:09:17 +0800

    demo/main.c

--- demo/main.c
+++ demo/main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <unistd.h>
 
+int call()
+{
+    return 0;
+}
+void printf_star()
+{
+    int i=0;
+    while (1){
+        if (i%2==0){
+            printf(" *** *** \n");
+            printf("*********\n");
+            printf(" *******\n");
+            printf(" ****\n");
+            printf(" **\n");
+        } else{
+            printf("\n");
+            printf(" *** *** \n");
+            printf("*********\n");
+            printf(" *******\n");
+            printf(" ****\n");
+            printf(" **\n");
+        }
+        sleep(1);
+        i++;
+        system("cls");
+    }
+}
 int main() {
-    printf("Hello, World!\n");
+    //call();
+    //printf("Hello, World!\n");
+    //printf_star();
+    int number;
+    int value;
+    number=22;
+    value=66;
+    printf("&number=%p\n",&number);
+    printf("&value=%p",&value);
     return 0;
 }
